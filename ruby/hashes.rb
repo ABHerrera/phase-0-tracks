4ae48d3b65b1045c7c937853a18a5553a03f0496
ABHerrera <adriana@openbubbles.com>
2016-09-20 15:10:18 -0700

    Added designer intake questions

---
+++ ruby/hashes.rb
@@ -0,0 +1,18 @@
+# Create program that askes a designers client:
+	# What is your name?
+	# How many children do you have?
+	# Do you like contemporary or traditional decor?
+	# What is your budget?
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How many children do you have?"
+number_of_chidren = gets.chomp
+
+puts "What style of beach decor do you prefer boho chic or sophisticated?"
+style = gets.chomp
+
+puts "What is your budget?"
+budget = gets.chomp
+
