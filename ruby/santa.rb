1e18c41daed64d91096f99d8b97b3bb0c0513afc
ABHerrera <adriana@openbubbles.com>
2016-09-27 14:00:02 -0700

    Initial commit

---
+++ ruby/santa.rb
@@ -0,0 +1,7 @@
+# Santa Class
+# Three instance methods
+# 1.) Speak method that prints "Ho, ho, ho! Haaaappy holidays!"
+# 2.) Eat milk and cookies method that method that takes a cookie type 
+#    (example: "snickerdoodle") as a parameter and prints "That was a good <type of cookie here>!" 
+# 3.) Initialize method that prints "Initializing Santa instance ...".
+
