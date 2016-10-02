42c988148cd95139bcb82a7be66864461e60b3a1
ABHerrera <adriana@openbubbles.com>
2016-10-02 10:23:50 -0700

    Added three attribute changing methods and two getter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,16 +15,7 @@ class Santa
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
-	# def age
-	# 	if age == ''
-	# 		age = 0
-	# 		puts "Santa is shy about his age."
-	# 	else
-	# 		puts "Santa is #{age} years old."
-	# 	end
-	# end
-
-	def Eat_milk_and_cookies(cookie_type)
+	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type} cookie!"
 		cookie_type
 	end
@@ -35,9 +26,10 @@ class Santa
 	end
 
 	def age(integer)
-		@integer = 0
-		puts "Santa is #{@integer}'s old."
+		@age = integer 
+		puts "Santa is #{@age} years old."
 	end
+
 
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
@@ -45,16 +37,46 @@ class Santa
 		@ethnicity = ethnicity
 		puts "Santa is #{@gender} and #{@ethnicity}."
 	end
+
+	def celebrate_birthday
+		celebrate_birthday = @age + 1 
+		puts "Santa is now #{celebrate_birthday}."
+	end 
+
+
+
+
+
+
+	# def get_mad_at(reindeer_name)
+	# 	@reindeer_name = reindeer_name
+	# 	reindeer_array = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	# 	reindeer_array.first.shift.last
+	# end
+
+
+# getter methods making public data outside the class
+
+	def gender
+		@gender 
+	end
+	
+
+	def ethnicity
+		@ethnicity
+	end
+
 end
 
-#Christmas = Santa.new
+
+# # DRIVER CODE 1
+# Christmas = Santa.new
 
 # Christmas.speak
 # puts "What type of cookie should Santa eat?"
 # cookie_type = gets.chomp
 # Christmas.Eat_milk_and_cookies(cookie_type)
-
-
+# Christmas.initialize(male, white)
 
 #Update your Santa class with the following attributes:
 # gender, which will be a string passed in on initialization
@@ -63,12 +85,48 @@ end
 #   This is ruby not passed in on initialization; it simply gets a default value of ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 # age, which is not passed in on initialization and defaults to 0
 
+# # DRIVER CODE 2
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
 
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
+# DRIVER CODE 3
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+# DRIVER CODE 4
+# gender = ["Female", "Female", "Female", "Female"]
+
+# # we make an empty container for our puppy collection
+# santas = []
+
+# puts "Iterating through genders to create a quartet of women santas ..."
+
+# gender.each do |g|
+#   puts "Switching santa's sex from male to #{gender}. Santa is now #{gender}."
+#   santas << Santa.new(gender)
+#   puts "There are now #{santas.length} in the quartet."
+#   puts "----"
+# end
+
+# puts "Testing each Santa instance in the array to make sure it can speak ..."
+# Santas.each do |s|
+#   Santa.speak
+# end
+
+christmas = Santa.new("male","Italian")
+christmas.eat_milk_and_cookies("sugar")
+christmas.speak
+christmas.reindeer_ranking
+christmas.age(12)
+christmas.celebrate_birthday
+# christmas.get_mad_at("Vixen")
