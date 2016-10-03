37c19536a9c4bedf8bde794bf3c881820efc6081
ABHerrera <adriana@openbubbles.com>
2016-10-02 19:02:02 -0700

    Take and store word

--- ruby/wordgame/wordgame_spec.rb
+++ ruby/wordgame/wordgame_spec.rb
@@ -1,17 +1,13 @@
-require_relative 'wordgame'
 
 # Create ‘class WordGame’
-
-describe WordGame do
-  let(:wordgame) { WordGame.new }
-
 
 # Enter word to be guessed
   # get word
   # store word
 
-# Person guesses the word
-   # give guesser to input letter
+# Person guesses a letter and sees if the letter is in the word
+   # get person guessing the word to input letter
+   # store the letter
    # see if word includes guessed letter 
 
 #  Guesses are limited, and the number of guesses available is related to the length of the word.
@@ -19,13 +15,28 @@ describe WordGame do
   # Limit the number of guesses based on the length of the word
   # Count the number of guesses
 
-# Repeated guesses do not count against the user.
+# Repeated guesses do not count against the user
   # exclude letters already guessed from counter
 
 # The guessing player receives continual feedback on the current state of the word. So if the secret word is "unicorn", the user will start out seeing something like "_ _ _ _ _ _ _", which would become "_ _ _ c _ _ _" after the user enters a guess of "c".
    # show "_ _ _ _ _ _ _"
    # replace characters with correctly guessed letters
    
-# The user should get a congratulatory message if they win, and a taunting message if they lose.
+# The user should get a congratulatory message if they win, and a taunting message if they lose
    # if user guesses the word they get a happy message
    # if user runs out of guesses they get a taunting loser message
+
+
+require_relative 'wordgame'
+
+describe WordGame do
+  let(:wordgame) { WordGame.new }
+
+  it "stores the word given on initialization" do
+  word = [ ]
+  expect(word.push(WordGame.new 'example1'))
+  end
+
+
+end
+
