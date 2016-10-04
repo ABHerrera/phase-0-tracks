c8b3eb931048a430c54eb81424d069bc0a717cac
ABHerrera <adriana@openbubbles.com>
2016-10-04 13:18:36 -0700

    Updated pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,2 +1,8 @@
-// Get string
-// To reverse the string move characters to their opposite location (i.e. flip their spot)
+// Revervse the a String:
+// Get a word/ string
+// Store the word/ string
+// Assess the number of characters of the word 
+// Assess the placement of the characters of the word
+// Shift each character to the character that is located directly opposite
+// print the string
+
