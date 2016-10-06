04b96a4d6a95d4f9f4a764b2c0b6fe2a22f2afb0
ABHerrera <adriana@openbubbles.com>
2016-10-05 21:46:53 -0700

    Initial commit

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// Write a function 
+// That takes an array of words
+// Returns the longest word in the array 
+// Example words = ["long phrase", "longest phrase", "longer phrase"]
+// Return "longest phrase"
+
+
