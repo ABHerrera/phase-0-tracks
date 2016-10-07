a881a3d3ea73e203503f8612ca403d7754ad3cc8
ABHerrera <adriana@openbubbles.com>
2016-10-07 15:30:15 -0700

    Combined arrays completed release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,13 @@ var names = ["Hyejin", "Adriana", "Tyler", "John"];
 
 colors.push("Purple");
 names.push("James");
+
+
+var horses = {}
+names.forEach(function(value, index) {
+
+  horses[value] = colors[index];
+
+});
+
+console.log(horses);
