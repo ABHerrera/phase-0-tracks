6ebde6b0e5b63f84771c0af3a637d60e1642df50
ABHerrera <adriana@openbubbles.com>
2016-10-10 00:53:14 -0700

    Completed Do the Thing

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,22 @@
+// DO THE THING 
 // Write a function 
 // That takes an array of words
-// Returns the longest word in the array 
+	// Measures each word for it's length 
+	// Returns the longest word
+		// Can use .length 
+		// Iterate over the array and see what they length of each word is
+		// use comparison logic to see which word has the most characters 
+		// print that word
 // Example words = ["long phrase", "longest phrase", "longer phrase"]
 // Return "longest phrase"
 
+// compareFunction info here: http://www.w3schools.com/jsref/jsref_sort.asp
+
+var words = ["long phrase", "longest phrase", "longer phrase"];
+var sorted = words.sort(function (a, b) { return b.length - a.length });
+
+console.log(sorted[0]);
+
+
+// RELEASE 1: KEY-VALUE MATCH
 
