f4eb168a61b7f95dadb95b868eec24c79613fbd3
ABHerrera <adriana@openbubbles.com>
2016-10-10 02:05:27 -0700

    Additional releases

--- js/algos.js
+++ js/algos.js
@@ -19,4 +19,36 @@ console.log(sorted[0]);
 
 
 // RELEASE 1: KEY-VALUE MATCH
+// write a function that takes two objects and checks to see if the objects share at least one key-value 
+// {name: "Steven", age: 54} and {name: "Tamir", age: 54} would equal 'true' because at least one key-value pair matches 
+
+var person1 = {name:"Steven", age: 54}
+var person2 = {name: "Tamir", age: 54}
+
+// Have to compare key1 to key2 and value1 to value2
+// manual way to check if key value pairs are equal
+
+console.log(person1 === person2);
+console.log(person1['name'] === person2['name']);
+console.log(person1['age'] === person2['age']);
+
+// Release 2: Generate Random Test Data
+// Write a function that takes an integer for length
+   // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+   // Source: https://www.freecodecamp.com/challenges/generate-random-whole-numbers-with-javascript
+   // http://www.w3schools.com/jsref/jsref_random.asp
+
+
+
+console.log(Math.floor((Math.random() * 10) + 1));
+
+// Builds and returns an array of strings of the given length.
+// Source to build random strings: http://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+
+
+// Example if input of 3 
+    // Out put of an array with 3 words 
+    // words don't have to be actual sensical English words
+    // Words should randomly vary in length
+       // minimum 1 letter, max 10 letters
 
