0395e7cd02804dc80bb440f4cb8d9e5e0324fd5a
ABHerrera <adriana@openbubbles.com>
2016-10-18 13:41:41 -0700

    Completed 6.4 with Standalone Module to a Mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,18 +1,48 @@
-# DO THE THING
-# Declare your module
+# Make a mixin version of the Shout module
+# Write two classes representing anything that might shout
+# Include the Shout module in those classes
+# Test work by adding Driver Code that instanitiate instances of your class and
+    # call the two module methods on each instance
 
-module Shout
-
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+module Yell
+  def yell_something(phrase)
+    puts "Whoa! Who is yelling '#{phrase.upcase}'?"
   end
-
-  def self.yelling_happily(words)
-  	words + "!!!!!!!" + ":)"
-  end
-
 end
 
-puts Shout::yell_angrily("WHAT ARE YOU DOING")
-puts Shout::yelling_happily("YIPPPEE")
+class Happy_Yelling
+	include Yell
+end
+
+class Mad_Yelling
+	include Yell
+end
+
+
+loud = Happy_Yelling.new
+loud.yell_something("Yippie!!!")
+
+loud = Mad_Yelling.new
+loud.yell_something("I won't vote for Donald Trump!")
+
+
+
+
+# # DO THE THING
+# # Declare your module
+
+# module Shout
+
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#   	words + "!!!!!!!" + ":)"
+#   end
+
+# end
+
+# puts Shout::yell_angrily("WHAT ARE YOU DOING")
+# puts Shout::yelling_happily("YIPPPEE")
 
