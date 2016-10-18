5617f2e477d05eb67bf7ae433c746bf5ededd4e1
ABHerrera <adriana@openbubbles.com>
2016-10-17 19:11:31 -0700

    Can pass gender and ethniticy into an array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,8 +2,27 @@
 
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
+#The @gender attribute should have a setter method that allows 
+#@gender to be reassigned from outside the class definition.
+
+	def gender=(gender) 
+		@gender = gender
+	end
+
+	def ethnicity=(ethnicity)
+		@ethnicity = ethnicity
+	end
+
+	def age
+		@age
 	end
 
 	def speak
@@ -18,6 +37,16 @@ end
 
 # DRIVER CODE
 
-holidays = Santa.new
-holidays.speak
-holidays.eat_milk_and_cookies("sugar")
+# holidays = Santa.new
+# holidays.speak
+# holidays.eat_milk_and_cookies("sugar")
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+p santas
