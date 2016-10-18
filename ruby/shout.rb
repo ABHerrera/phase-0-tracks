0159f9c8962dced3c04f9a51b925e84d4e80d3dd
ABHerrera <adriana@openbubbles.com>
2016-10-18 13:13:38 -0700

    Refactor 6.4 Initial Commit

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,37 +1,6 @@
-# module Shout
-# 	# we'll put some methods here soon, but this code is fine for now!
-# 	def self.yell_angrily(words)
-# 		words + "!!!" + " :("
-# 	end
-
-# 	def self.yelling_happily(words)
-# 		words + "!!!" + " :)"
-# 	end
-# end 
-
-# puts Shout::yell_angrily("yo")
-# puts Shout::yelling_happily("momma")
-
+# DO THE THING
+# Declare your module
 
 module Shout
-	def yell(person, words)
-		puts "The #{person} is yelling '#{words}!!!'. How rude."
-	end
+  # we'll put some methods here soon, but this code is fine for now!
 end
-
-class Child
-	include Shout
-end
-
-class Old_Man
-	include Shout
-end
-
-child = Child.new
-child.yell("child", "I WANT SOME ICE CREAM")
-
-old_man = Old_Man.new
-old_man.yell("old man", "YOU WHIPPER SNAPPER")
-
-
-#
