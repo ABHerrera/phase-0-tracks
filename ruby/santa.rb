99539dc38af8191f52b761d75db992033a230f7c
ABHerrera <adriana@openbubbles.com>
2016-10-17 18:35:04 -0700

    Added speak, initialize, and eat_milk_and_cookie instance methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,23 @@
 # Refactor
 
 class Santa
+
+	def initialize
+		puts "Initializing Santa instance ..."
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type} cookie." 
+	end
+
 end
 
+# DRIVER CODE
+
 holidays = Santa.new
+holidays.speak
+holidays.eat_milk_and_cookies("sugar")
