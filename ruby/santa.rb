d249fac1ffc5579687b5143e5ee8e2e1c7c8dfb8
ABHerrera <adriana@openbubbles.com>
2016-10-22 14:01:37 -0700

    Added instance methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,52 +1,29 @@
-# Refactor
+# define a Santa class
+
+# add three instance methods:
+  # a speak method that prints "Ho, ho, ho! Haaaappy holidays!
+  # an eat_milk_and_cookies method that take a cookie type and prints "That was a good <type of cookie here>!" 
+  # An initialize method that prints "Initializing Santa instance ...".
+
 
 class Santa
-
-	def initialize(gender, ethnicity)
+	def initialize
 		puts "Initializing Santa instance ..."
-		@gender = gender
-		@ethnicity = ethnicity
-		@reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
-	end
-
-#The @gender attribute should have a setter method that allows 
-#@gender to be reassigned from outside the class definition.
-
-	def gender=(gender) 
-		@gender = gender
-	end
-
-	def ethnicity=(ethnicity)
-		@ethnicity = ethnicity
-	end
-
-	def age
-		@age
 	end
 
 	def speak
-		puts "Ho, ho, ho! Haaaappy holidays!"
+		puts "Ho, ho, ho! Haaaappy holidays!" 
 	end
 
 	def eat_milk_and_cookies(cookie_type)
-		puts "That was a good #{cookie_type} cookie." 
-	end
+		puts "That was a good #{cookie_type} cookie!" 
+	end 
 
 end
 
-# DRIVER CODE
 
-# holidays = Santa.new
-# holidays.speak
-# holidays.eat_milk_and_cookies("sugar")
+santa = Santa.new
+santa.eat_milk_and_cookies("sugar")
 
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
-p santas
+
+# instance variables have an interesting scope where they can be accessed in instance methods
