eaa93c2d7b242809c982e20f23c563f4971eae59
ABHerrera <adriana@openbubbles.com>
2016-10-22 14:08:48 -0700

    Added gender, ethinicity, reindeer_ranking, and age attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,10 +5,21 @@
   # an eat_milk_and_cookies method that take a cookie type and prints "That was a good <type of cookie here>!" 
   # An initialize method that prints "Initializing Santa instance ...".
 
+# Add attributes
+  # gender, passed on initialization
+  # ethnicity, passed on initialization
+  # reindeer_ranking, an array of reindeer from most preferred to least preferred. 
+        # This is not passed in on initialization; it simply gets a default value of ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  # age, which is not passed in on initialization and defaults to 0
+
 
 class Santa
-	def initialize
+	def initialize(gender,ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = age
 	end
 
 	def speak
@@ -19,11 +30,30 @@ class Santa
 		puts "That was a good #{cookie_type} cookie!" 
 	end 
 
+	def age
+		@age = 0
+	end 
+
 end
 
 
-santa = Santa.new
-santa.eat_milk_and_cookies("sugar")
+# DRIVER CODE 2
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+p santas
+
+
+# DRIVER CODE 1
+# santa = Santa.new
+# santa.eat_milk_and_cookies("sugar")
+
 
 
 # instance variables have an interesting scope where they can be accessed in instance methods
