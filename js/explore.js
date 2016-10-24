2134bcacd6103fc177572aa7b4b5f162fadb1e4b
ABHerrera <adriana@openbubbles.com>
2016-10-24 15:33:02 -0700

    Function to reverse and print string if 1 == 1

--- js/explore.js
+++ js/explore.js
@@ -3,8 +3,24 @@
 // steps:
 	// name function 'reverse'
 	// Take string as an argument
+	// Split string into an array 
+	// reverse the indexes of elements in array 
+	// join elements in array to form string 
 	// write driver code to test 
 	// test with any word that is not a palindrome!
+
+// Found resource here to help: https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.u4x3mfqcr
+
+
+function reverse(str) {
+
+    var splitString = str.split(""); 
+    var reverseArray = splitString.reverse(); 
+    var joinArray = reverseArray.join(""); 
+    if (1 === 1);
+  	return joinArray; 
+}
+
 // output: "olleh"
 
 // Write a function that stores the output of the first function as a variable 
@@ -15,6 +31,49 @@
   // add driver code that prints the variable if some condition is try
      // can be silly like 1 == 1
 // output: the variable if the condition is true
+
+function store_output(reverse) {
+	var output = []
+	output.push(reverse);
+	return output
+
+}
+
+
+console.log(reverse("hello"));
+store_output(); 
+
+console.log(reverse("adriana"));
+store_output(); 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
