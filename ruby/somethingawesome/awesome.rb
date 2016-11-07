811584a713fd42d5284a91c3e3a8a7bc4b33f3b4
ABHerrera <adriana@openbubbles.com>
2016-11-07 14:28:04 -0800

    Creating values class and asking how many values to track

---
+++ ruby/somethingawesome/awesome.rb
@@ -0,0 +1,56 @@
+# Mood Meter
+  # Application that asks the user what their top business and life values are
+  	# Ask user what their values are exit when done
+  	# Store the data in a hash 
+
+  # Ask the user on a daily basis how they are doing on their values
+    # Store the data 
+    # If the data is under a certain threshold recommend options to get back aligned
+
+  # Ask the user what they are grateful for that day and store the data 
+
+
+# BUSINESS LOGIC 
+
+class Values
+# Take and store the users values 
+
+	def 
+
+end 
+
+
+
+# USER INTERFACE LOGIC 
+
+
+counter = 0
+
+puts "How many values would you like to track?"
+values = gets.chomp.to_i
+
+# until counter == input
+values.times do |counter|
+	counter += 1
+	p counter
+	values
+end
+
+
+
+
+
+
+
+
+
+
+# Write a Ruby program that uses persistent data to make your life better, 
+   # or makes someone else's life better. Find several opportunities to try 
+   # something not explicitly shown in the video. Can users create data? 
+   # Retrieve it? Manipulate it? It's up to you. Push yourself, and decide with 
+   # integrity when you are finished. Note that you do not need to use classes 
+   # in this assignment -- keeping a class instance and a database row in sync is a 
+   # lot of work, so that might be too much to tackle.
+
+# 
