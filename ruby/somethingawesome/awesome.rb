cf7b5410254caf6b6d7d792037a58525e203a4da
ABHerrera <adriana@openbubbles.com>
2016-11-07 18:46:06 -0800

    Joining files for persistant data

--- ruby/somethingawesome/awesome.rb
+++ ruby/somethingawesome/awesome.rb
@@ -1,40 +1,8 @@
-# Mood Meter
-  # Application that asks the user what their top business and life values are
-  	# Ask user what their values are exit when done
-  	# Store the data in a hash 
+require 'sqlite3'
 
-  # Ask the user on a daily basis how they are doing on their values
-    # Store the data 
-    # If the data is under a certain threshold recommend options to get back aligned
-
-  # Ask the user what they are grateful for that day and store the data 
-
-
-# BUSINESS LOGIC 
-
-class Values
-# Take and store the users values 
-
-	def 
-
-end 
-
-
-
-# USER INTERFACE LOGIC 
-
-
-counter = 0
-
-puts "How many values would you like to track?"
-values = gets.chomp.to_i
-
-# until counter == input
-values.times do |counter|
-	counter += 1
-	p counter
-	values
-end
+# create SQLite3 database
+db = SQLite3::Database.new("daily_gratitude.db")
+# db.results_as_hash = true
 
 
 
@@ -45,12 +13,74 @@ end
 
 
 
-# Write a Ruby program that uses persistent data to make your life better, 
-   # or makes someone else's life better. Find several opportunities to try 
-   # something not explicitly shown in the video. Can users create data? 
-   # Retrieve it? Manipulate it? It's up to you. Push yourself, and decide with 
-   # integrity when you are finished. Note that you do not need to use classes 
-   # in this assignment -- keeping a class instance and a database row in sync is a 
-   # lot of work, so that might be too much to tackle.
 
-# 
+
+
+
+
+
+
+
+
+
+
+
+# class Gratitude
+
+# 	def initialize(date)
+# 		@date = date
+# 		@daily_reflection = {}
+# 		@grateful = []
+
+# 	end 
+
+# 	def daily_gratitude
+
+# 		while true
+
+# 			puts "What are you grateful for? If done enter 'exit'."
+# 			grateful_for = gets.chomp.downcase 
+
+# 			if grateful_for == "exit"	
+# 				exit
+# 			else
+# 				@daily_reflection[@date] = @grateful.push(grateful_for)
+# 			end
+# 		end
+
+# 	end 
+
+# 	def print_gratitude
+# 		"Here are the things you are grateful for to date:"
+# 		@daily_reflection.each_pair { |k, v| puts "Date: #{k}, Grateful for: #{v}" }
+# 	end 
+
+
+# end
+
+
+# puts "Great to see you again!"
+# puts "Please enter the date:"
+# date = gets.chomp 
+
+# grateful = Gratitude.new date
+# p grateful.daily_gratitude
+# p grateful.print_gratitude
+
+
+
+
+
+
+
+# # week = {
+# # 	:Sunday => {},
+# # 	:Monday => {},
+# # 	:Tuesday => {},
+# # 	:Wednesday => {},
+# # 	:Thursday => {},
+# # 	:Friday => {},
+# # 	:Saturday => {},
+# # } 
+
+
